4ad6989aa4eb606d6048033dfd7dee87a7fd47cd
spyoungtech <spenceryoung@ufl.edu>
2016-11-16 00:20:09 -0500

    version 0.1a
    First release

--- pyfilehash.py
+++ pyfilehash.py
@@ -1,20 +1,36 @@
-import easygui as eg
-import os
+import argparse
 from hashlib import md5, sha1, sha224, sha256, sha384
+import os
 
-PROGRESS = 0
+import easygui as eg
 
-box = eg.buttonbox(msg="Progress... {}%".format(PROGRESS), choices=["Cancel"], cancel_choice="Cancel", run=False)
-box.ui.boxRoot.iconbitmap(r'C:\Python27\Lib\site-packages\easygui-0.97.4-py2.7.egg\easygui\favicon.ico')
+parser = argparse.ArgumentParser()
+parser.add_argument("algorithm")
+parser.add_argument("path")
+args = parser.parse_args()
+
+hash_dict = {"md5": md5,
+             "sha1": sha1,
+             "sha224": sha224,
+             "sha256": sha256,
+             "sha384": sha384}
+
+if args.algorithm.lower() not in hash_dict:
+    raise ValueError("Invalid algorithm '{}'".format(args.algorithm))
+
+assert os.path.isfile(args.path)
+
+hash_func = hash_dict[args.algorithm.lower()]
+
+box = eg.buttonbox(msg="Loading...\nProgress: 0%", title="PyFileHash", choices=["Cancel"], cancel_choice="Cancel", run=False)
+#icon_path = os.path.join(__file__, "icon.ico")
+#box.ui.boxRoot.iconbitmap(icon_path)
 
 class FileLoader(object):
-    def __init__(self, fp, chunk_size=None):
+    def __init__(self, fp):
         self.fp = fp
         self.file_size = os.stat(self.fp).st_size
-        if chunk_size:
-            self.chunk_size = chunk_size
-        else:
-            self.chunk_size = int(self.file_size / 20)+1
+        self.chunk_size = int(self.file_size / 20)+1
         self.file = open(self.fp, 'rb')
         self.progress = 0
         self.data = b''
@@ -26,7 +42,7 @@ class FileLoader(object):
             if next_data:
                 self.progress += self.progress_incr
                 self.data += next_data
-                box.msg = "Progress... {}%".format(int(self.progress))
+                box.msg = "Loading...\nProgress: {}%".format(int(self.progress))
                 yield next_data
             else:
                 box.msg = "Complete!"
@@ -34,28 +50,26 @@ class FileLoader(object):
     def read(self):
         try:
             next_chunk = next(self.reader)
-            #self.data += next_chunk
             box.ui.boxRoot.after(0, self.read())
         except StopIteration:
-            #box.ui.boxRoot.after(0, box.stop)
-            #box.ui._choices.append("MD5")
-            #box.ui.create_buttons(box.ui._choices, box.ui._default_choice)
-            return None
+            try:
+                box.ui.boxRoot.destroy()
+            except:
+                pass
 
 
-#h_dict = {"MD5": md5}
 
-f = FileLoader("allitems.xml")
-box.ui.boxRoot.after(100, f.read())
-#hash_choice = box.run()
-#hash_func = h_dict[hash_choice]
-#result = hash_func(f.data).hexdigest()
-
+f = FileLoader(args.path)
+box.ui.boxRoot.after(0, f.read())
 try:
     box.run()
-except AttributeError:
+except:
     pass
 
+result = hash_func(f.data).hexdigest()
 
-eg.msgbox(msg=result)
-print(result)
+message = """Results for {}:
+{}: {}
+""".format(os.path.abspath(args.path), args.algorithm.upper(), result)
+
+eg.msgbox(msg=message, title="PyFileHash")
