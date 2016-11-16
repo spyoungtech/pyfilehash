047aa5111764e824280b7338abd5046b22d6c9b1
spyoungtech <spenceryoung@ufl.edu>
2016-11-16 00:49:46 -0500

    support to suppress GUI
    Some CLI options

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# pyfilehash
+# PyFileHash
+
+PyFileHash is a utility for getting file checksums.
+Supports md5, sha1, sha224, sha256, sha384, and sha512
--- pyfilehash.py
+++ pyfilehash.py
@@ -1,28 +1,31 @@
 import argparse
-from hashlib import md5, sha1, sha224, sha256, sha384
+from hashlib import md5, sha1, sha224, sha256, sha384, sha512
 import os
 
-import easygui as eg
 
 parser = argparse.ArgumentParser()
-parser.add_argument("algorithm")
-parser.add_argument("path")
+parser.add_argument("algorithm", help="The algorithm to use, I.E MD5, SHA1, SHA256, etc.",)
+parser.add_argument("path", help="The path of the file. Can be relative or absolute.")
+parser.add_argument("-q", "--quiet", action="store_true", help="Quiet flag. Supresses GUI")
 args = parser.parse_args()
 
 hash_dict = {"md5": md5,
              "sha1": sha1,
              "sha224": sha224,
              "sha256": sha256,
-             "sha384": sha384}
+             "sha384": sha384,
+             "sha512": sha512}
 
 if args.algorithm.lower() not in hash_dict:
     raise ValueError("Invalid algorithm '{}'".format(args.algorithm))
 
-assert os.path.isfile(args.path)
+if not os.path.isfile(args.path):
+    raise ValueError("Invalid file path: '{}'\nMust be relative or absolute path to a file.".format(args.path))
 
 hash_func = hash_dict[args.algorithm.lower()]
-
-box = eg.buttonbox(msg="Loading...\nProgress: 0%", title="PyFileHash", choices=["Cancel"], cancel_choice="Cancel", run=False)
+if not args.quiet:
+    import easygui as eg
+    box = eg.buttonbox(msg="Loading...\nProgress: 0%", title="PyFileHash", choices=["Cancel"], cancel_choice="Cancel", run=False)
 #icon_path = os.path.join(__file__, "icon.ico")
 #box.ui.boxRoot.iconbitmap(icon_path)
 
@@ -41,35 +44,45 @@ class FileLoader(object):
             next_data = self.file.read(self.chunk_size)
             if next_data:
                 self.progress += self.progress_incr
-                self.data += next_data
-                box.msg = "Loading...\nProgress: {}%".format(int(self.progress))
+                if not args.quiet:
+                    box.msg = "Loading...\nProgress: {}%".format(int(self.progress))
                 yield next_data
             else:
-                box.msg = "Complete!"
+                if not args.quiet:
+                    box.msg = "Complete!"
                 break
     def read(self):
-        try:
-            next_chunk = next(self.reader)
-            box.ui.boxRoot.after(0, self.read())
-        except StopIteration:
+        if not args.quiet:            
             try:
-                box.ui.boxRoot.destroy()
-            except:
-                pass
+                next_chunk = next(self.reader)
+                self.data += next_chunk
+                box.ui.boxRoot.after(0, self.read())
+            except StopIteration:
+                try:
+                    box.ui.boxRoot.destroy()
+                except:
+                    pass
+        else:
+            for chunk in self.reader:
+                self.data += chunk
 
 
 
 f = FileLoader(args.path)
-box.ui.boxRoot.after(0, f.read())
-try:
-    box.run()
-except:
-    pass
+if not args.quiet:
 
+    box.ui.boxRoot.after(0, f.read())
+    try:
+        box.run()
+    except:
+        pass
+else:
+    f.read()
 result = hash_func(f.data).hexdigest()
 
 message = """Results for {}:
 {}: {}
 """.format(os.path.abspath(args.path), args.algorithm.upper(), result)
-
-eg.msgbox(msg=message, title="PyFileHash")
+print(message)
+if not args.quiet:
+    eg.msgbox(msg=message, title="PyFileHash")
